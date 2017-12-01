77ebead422749e712002022ba5255ab22fcc1150
Burger <wrz641@ku.dk>
2017-12-01 13:58:23 +0100

    e

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,8 +1,8 @@
 d3.select(window).on('load', init);
 
 function init() {
-  d3.csv(
-    'data.csv', 
+  d3.text(
+    'station.txt', 
     function(error, data) {
       if (error) throw error;
       console.log(data);
