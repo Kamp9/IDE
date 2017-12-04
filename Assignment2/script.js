cf955e20965c763c64fefa1fe7d125cd39d3aa82
Burger <wrz641@ku.dk>
2017-12-04 09:44:34 +0100

    øv

--- Assignment2/script.js
+++ Assignment2/script.js
@@ -1,10 +1,72 @@
-d3.select(window).on('load', init);
 
-function init() {
-  d3.text(
-    'station.txt', 
-    function(error, data) {
-      if (error) throw error;
-      console.log(data);
-    });
-}
+
+//d3.text("station.csv", function(datasetText) {
+//var parsedCSV = d3.csvParseRows(datasetText);
+
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 80, bottom: 30, left: 50},
+    width = svg.attr("width") - margin.left - margin.right,
+    height = svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+  var xScale = d3.scaleLinear()
+      .domain([0,
+               d3.max(parsedCSV, 
+                      function(d){
+                        return d[0];
+                      })])
+      .range([padding,width-padding]);
+
+  var yScale = d3.scaleLinear()
+      .domain([d3.min(parsedCSV,
+                      function(d){
+                        return d[1];
+                      }),
+               d3.max(parsedCSV, 
+                      function(d){
+                        return d[1];
+                      })])
+      .range([height-padding, padding]);
+
+
+
+var line = d3.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.close); });
+
+d3.csv("stations.csv", function(d) {
+  d.date = parseTime(d.date);
+  d.close = +d.close;
+  return d;
+}, function(error, data) {
+  if (error) throw error;
+
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+  y.domain(d3.extent(data, function(d) { return d.close; }));
+
+  g.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+    .select(".domain")
+      .remove();
+
+  g.append("g")
+      .call(d3.axisLeft(y))
+    .append("text")
+      .attr("fill", "#000")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Price ($)");
+
+  g.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("stroke-width", 1.5)
+      .attr("d", line);
+});
