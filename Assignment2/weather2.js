ff07f383fa174ee8d41935e9826697396c67d7db
Burger <wrz641@ku.dk>
2017-12-05 10:18:29 +0100

    n

--- Assignment2/weather2.js
+++ Assignment2/weather2.js
@@ -1,8 +1,21 @@
-var svg = d3.select("svg"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var margin = {top: 20, right: 20, bottom: 30, left: 30},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+
+var svg = d3.select("body")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom),
+    g = svg.append("g")
+        .attr("transform", 
+              "translate(" + margin.left + "," + margin.top + ")");
+
+//var svg = d3.select("svg"),
+//    margin = {top: 20, right: 20, bottom: 30, left: 40},
+//    width = +svg.attr("width") - margin.left - margin.right,
+//    height = +svg.attr("height") - margin.top - margin.bottom,
+//    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var x = d3.scaleBand()
     .rangeRound([0, width])
@@ -15,7 +28,7 @@ var y = d3.scaleLinear()
 var z = d3.scaleOrdinal()
     .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-d3.csv("data.csv", function(d, i, columns) {
+d3.csv("station.csv", function(d, i, columns) {
   for (i = 1, t = 0; i < columns.length; ++i) t += d[columns[i]] = +d[columns[i]];
   d.total = t;
   return d;
