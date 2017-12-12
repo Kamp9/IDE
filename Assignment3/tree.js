706a0edc4d23b509e189b7f65eeb76c52667245e
Burger <wrz641@ku.dk>
2017-12-12 09:33:43 +0100

    røv

--- Assignment3/tree.js
+++ Assignment3/tree.js
@@ -1,11 +1,12 @@
-treeJson = d3.json("tree.json", function(error, treeData) {
+treeJson = d3.json("donald2.json", function(error, treeData) {
     dTree.init(treeData, {
         target: "#graph",
         debug: true,
         height: 800,
-        width: 1200,
+        width: screen.width,//1400,
         callbacks: {
             nodeClick: function(name, extra) {
+                window.open("http://www.google.com/search?q="+name);
                 console.log(name);
             }
         }
