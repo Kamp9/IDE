f933a06c3f4db08ddc551ffaf6e4cb0e36b0773a
Burger <wrz641@ku.dk>
2018-01-11 14:24:50 +0100

    style

--- exam/system.js
+++ exam/system.js
@@ -1,12 +1,7 @@
-var w = 800, h = 600;
+var w = 1200, h = 1200;
 var t0 = Date.now();
 
-var planets = [
-  { R: 300, r:  5, speed: 5, phi0: 90},
-  { R: 150, r: 10, speed: 2, phi0: 190}
-];
-
-d3.csv("planets.csv",function(error, data) {
+d3.csv("habit_planets.csv",function(error, data) {
   if (error) throw error;
 
 
@@ -18,6 +13,8 @@ d3.csv("planets.csv",function(error, data) {
     d['P. Radius (EU)'] = + d['P. Radius (EU)']
     d['P. Ts Mean (K)'] = + d['P. Ts Mean (K)']
     d['P. Mean Distance (AU)'] = + d['P. Mean Distance (AU)']
+    d['P. Period (days)'] = + d['P. Period (days)']
+    d.phi0 = 190
   });
   console.log(data);
 
@@ -31,18 +28,22 @@ svg.append("circle").attr("r", 20).attr("cx", w/2)
 var container = svg.append("g")
 .attr("transform", "translate(" + w/2 + "," + h/2 + ")")
 
-container.selectAll("g.planet").data(planets).enter().append("g")
-  .attr("class", "planet").each(function(d, i) {
-    d3.select(this).append("circle").attr("class", "orbit")
-      .attr("r", d.R);
-    d3.select(this).append("circle").attr("r", d.r).attr("cx",d.R)
+container.selectAll("g.planet").data(data).enter().append("g")
+    .on("mouseover", function(d) {
+        console.log(d['P. Name']);
+        // show facts in textbox
+      })
+    .attr("class", "planet").each(function(d, i) {
+    //d3.select(this).append("circle").attr("class", "orbit")
+    //  .attr("r", d['P. Mean Distance (AU)']*1000);
+    d3.select(this).append("circle").attr("r", d['P. Radius (EU)']*3).attr("cx",d['P. Mean Distance (AU)']*1000)
       .attr("cy", 0).attr("class", "planet");
   });
 
 d3.timer(function() {
   var delta = (Date.now() - t0);
   svg.selectAll(".planet").attr("transform", function(d) {
-    return "rotate(" + d.phi0 + delta * d.speed/200 + ")";
+    return "rotate(" + d.phi0 + delta * d['P. Period (days)']/2000 + ")";
   });
 });
 
