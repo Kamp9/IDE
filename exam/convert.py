9a2673c2addc604cc969ab7fd527e63f36e8abed
troels <kamp991@gmail.com>
2018-01-11 14:19:17 +0100

    lidt mere converter

--- exam/convert.py
+++ exam/convert.py
@@ -10,17 +10,18 @@ extract = [0, 1, 3, 5, 9, 11, 13, 22, 27, 30, 36, 63, 67]
 
 dataTosave = my_data[:, extract]
 
-all = []
+allp = []
 habit = []
 
 for i in dataTosave:
     temp = [number for number in i if number]
     if temp[0] in my_names:
-        habit += [temp]
+        habit += [[temp[0]] + ["empty"] + temp[1:]]
     elif temp[1] in my_names:
         habit += [temp]
     if len(temp) == len(extract):
-        all += [temp]
+        allp += [temp]
 
-np.savetxt("planets.csv", np.array(all),fmt='%s',delimiter=",")
-np.savetxt("habit_planets.csv", np.array(habit),fmt='%s',delimiter=",")
+
+np.savetxt("habit_planets.csv", [allp[0]] + habit, fmt='%s', delimiter=",")
+np.savetxt("planets.csv", allp, fmt='%s', delimiter=",")
