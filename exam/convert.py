bb3dde70a5c2fe3f96ff158ec4de5ef8333426f7
troels <kamp991@gmail.com>
2018-01-11 13:07:12 +0100

    convert

--- exam/convert.py
+++ exam/convert.py
@@ -5,6 +5,15 @@ np.set_printoptions(threshold=np.nan)
 
 my_data = genfromtxt('phl_hec_all_kepler.csv', delimiter=',', dtype=object)
 
-dataTosave =  my_data[0:23, [0, 3, 5, 9, 11, 13, 22, 27, 30, 36, 63, 67]]
+extract = [0, 3, 5, 9, 11, 13, 22, 27, 30, 36, 63, 67]
 
-np.savetxt("planets.csv",dataTosave,fmt='%s',delimiter=",")
+dataTosave = my_data[:, extract]
+
+ans = []
+for i in dataTosave:
+    temp = [number for number in i if number]
+    if len(temp) == len(extract):
+        ans += [temp]
+# print ans
+
+np.savetxt("planets.csv", np.array(ans),fmt='%s',delimiter=",")
