c1aaa9890c7c0520ad7f48f080389e2c9220a6a4
troels <kamp991@gmail.com>
2018-01-11 13:42:13 +0100

    habit

--- exam/convert.py
+++ exam/convert.py
@@ -4,16 +4,23 @@ from numpy import genfromtxt
 np.set_printoptions(threshold=np.nan)
 
 my_data = genfromtxt('phl_hec_all_kepler.csv', delimiter=',', dtype=object)
+my_names = genfromtxt('exonames.txt', delimiter=',', dtype=str)
 
-extract = [0, 3, 5, 9, 11, 13, 22, 27, 30, 36, 63, 67]
+extract = [0, 1, 3, 5, 9, 11, 13, 22, 27, 30, 36, 63, 67]
 
 dataTosave = my_data[:, extract]
 
-ans = []
+all = []
+habit = []
+
 for i in dataTosave:
     temp = [number for number in i if number]
+    if temp[0] in my_names:
+        habit += [temp]
+    elif temp[1] in my_names:
+        habit += [temp]
     if len(temp) == len(extract):
-        ans += [temp]
-# print ans
+        all += [temp]
 
-np.savetxt("planets.csv", np.array(ans),fmt='%s',delimiter=",")
+np.savetxt("planets.csv", np.array(all),fmt='%s',delimiter=",")
+np.savetxt("habit_planets.csv", np.array(habit),fmt='%s',delimiter=",")
