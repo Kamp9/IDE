30c9f793a0bdffda37716ea10bb671e001bcefa8
troels <kamp991@gmail.com>
2018-01-11 10:56:47 +0100

    init2

--- exam/convert.py
+++ exam/convert.py
@@ -1,5 +1,9 @@
+import numpy as np
 from numpy import genfromtxt
+
+np.set_printoptions(threshold=np.nan)
 
 my_data = genfromtxt('phl_hec_all_kepler.csv', delimiter=',', dtype=object)
 
-print my_data[1][0]
+print my_data[1:, [0, 3, 5, 9, 11, 13, 22, 30, 36, 63, 67]]
+print len(my_data[1:][0])
