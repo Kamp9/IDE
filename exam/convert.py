7e6cd9cf3615c96513aeab24c34803bfff6633b7
troels <kamp991@gmail.com>
2018-01-15 10:55:30 +0100

    convert

--- exam/convert.py
+++ exam/convert.py
@@ -8,7 +8,7 @@ my_data = genfromtxt('phl_hec_all_kepler.csv', delimiter=',', dtype=object)
 my_data2 = genfromtxt('phl_hec_all_confirmed.csv', delimiter=',', dtype=object)
 my_names = genfromtxt('exonames.txt', delimiter=',', dtype=str)
 
-extract = [0, 1, 3, 5, 9, 11, 13, 22, 27, 30, 36, 38, 39, 63, 67]
+extract = [0, 1, 3, 5, 9, 11, 13, 22, 27, 30, 33, 36, 38, 39, 63, 67]
 
 dataTosave = my_data[:, extract]
 dataTosave2 = my_data2[:, extract]
@@ -32,16 +32,13 @@ for i in dataTosave2:
         allp2 += [temp]
 
 tmp = np.array(habit)
-norm_habit = normalize(tmp[:, 9], norm='max')
+norm_habit = normalize(tmp[:, 9].reshape(1, -1), norm='max')
 for i, e in enumerate(habit):
     tmp = norm_habit[0][i].astype('|S10')
     e.append(tmp)
 
 allp_copy = allp[0]
-
-print allp_copy
 allp_copy.append('Norm Distance')
-print allp_copy
 
 
 np.savetxt("habit_planets.csv", [allp_copy] + habit, fmt='%s', delimiter=",")
