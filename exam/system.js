637adbe06e992ad82ce2c312101f851f6fdd6e21
troels <kamp991@gmail.com>
2018-01-17 15:52:18 +0100

    ...

--- exam/system.js
+++ exam/system.js
@@ -23,6 +23,7 @@ function data_changer(inp){
 
 
 
+
 d3.csv("planets.csv",function(error, data2) {
     if (error) throw error;
     var t_min2 = Infinity;
@@ -284,23 +285,6 @@ d3.timer(function() {
   });
 });
 
-var defs = svg.append("defs");
-
-var linearGradient = defs.append("linearGradient")
-    .attr("id", "linear-gradient");
-
-//A color scale
-    var colorScale = d3.scaleLinear()
-        .range(["#2c7bb6", "#00a6ca","#00ccbc","#90eb9d","#ffff8c","#f9d057","#f29e2e","#e76818","#d7191c"]);
-
-//Append multiple color stops by using D3's data/enter step
-    linearGradient.selectAll("stop")
-        .data( colorScale.range())
-        .enter().append("stop")
-        .attr("offset", function(d,i) { return i/(colorScale.range().length-1); })
-        .attr("stop-color", function(d) { return d; })
-        .style("fill", "url(#linear-gradient)");
-
 
 change_dataset(1);
 
@@ -415,5 +399,7 @@ else if (window.attachEvent) {
 else {
     window["onload"] = sse50.initMenu;
 }
+
+
 
 })})});
